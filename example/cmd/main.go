8d91ed7518e7ecba93df7bc4ef653bb777c43954
Friedrich Große <friedrich.grosse@gmail.com>
2015-07-20 22:35:02 +0200

    Fix broken travis build

--- example/cmd/main.go
+++ example/cmd/main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"github.com/fgrosse/servo"
 	"github.com/fgrosse/servo/example"
+	"github.com/fgrosse/servo/configuration"
 )
 
 func main() {
-	config := servo.NewYAMLFileLoader("config.yml")
+	config := configuration.NewYAMLFileLoader("config.yml")
 	kernel := servo.NewKernel(config)
 	example.RegisterTypes(kernel.TypeRegistry)
 	kernel.Run()

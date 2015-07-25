5deefad858e30cf792f96526094be52bf282fe2a
Friedrich Große <friedrich.grosse@gmail.com>
2015-07-25 13:14:22 +0200

    Use new goldi func types for http handler

--- example/http_handler.go
+++ example/http_handler.go
@@ -2,12 +2,6 @@ package example
 
 import "net/http"
 
-type MySimpleHandler struct {}
-
-func NewMySimpleHandler() *MySimpleHandler {
-	return &MySimpleHandler{}
-}
-
-func (c *MySimpleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello servo world :)"))
 }
